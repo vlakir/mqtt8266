b0a26463b3faffbb52f7e3e365ad61a46d63a667
vlakir <vek2006_73@mail.ru>
2019-02-20 14:34:48 +0300

    imroving mqqt code

--- mqtt_utils.cpp
+++ mqtt_utils.cpp
@@ -6,36 +6,39 @@ Timer xPostStateADC, xPostStateGPIO;
 static WiFiClient xWifiClient;
 static PubSubClient xPsClient(xWifiClient);
 
-void vSetServer() {
+
+
+
+void vServerReconnect() {
+	xPsClient.disconnect();
 	xPsClient.setServer(xGlobalSettings.acMQTTserver, xGlobalSettings.uiMQTTport);
+	
+	xPsClient.setCallback(vRecieveCallback);
+
+	Serial.print("Attempting MQTT connection...");
+
+	// Attempt to connect
+	if (xPsClient.connect("arduinoClient", xGlobalSettings.acMQTTclientID, xGlobalSettings.acMQTTclientPassword)) {
+		Serial.println("connected");
+	}
+	else {
+		Serial.print("failed, rc=");
+		Serial.print(xPsClient.state());
+		Serial.println(" try again in 5 seconds");
+		// Wait 5 seconds before retrying
+		delay(5000);
+	}
+
+	char acTopic[80];
+	sprintf(acTopic, "%s%s", xGlobalSettings.acDeviceID, "/management/#");
+	xPsClient.subscribe(acTopic);
 }
 
 
 void vMqttLoop() {	
 		
 	while (!xPsClient.connected()) {
-			
-		vSetServer();
-		xPsClient.setCallback(vRecieveCallback);
-
-		Serial.print("Attempting MQTT connection...");
-
-		// Attempt to connect
-		if (xPsClient.connect("arduinoClient", xGlobalSettings.acMQTTclientID, xGlobalSettings.acMQTTclientPassword)) {
-			Serial.println("connected");
-		}
-		else {
-			Serial.print("failed, rc=");
-			Serial.print(xPsClient.state());
-			Serial.println(" try again in 5 seconds");
-			// Wait 5 seconds before retrying
-			delay(5000);
-		}
-
-		char acTopic[80];
-		sprintf(acTopic, "%s%s", xGlobalSettings.acDeviceID, "/management/#");
-		xPsClient.subscribe(acTopic);	
-
+		vServerReconnect();
 		xPostStateADC.every(ADC_CHECK_PERIOD_MS, vPostADC, (void *)&xPsClient);
 		xPostStateGPIO.every(GPIO_CHECK_PERIOD_MS, vPostGPIO, (void *)&xPsClient);
 	}
